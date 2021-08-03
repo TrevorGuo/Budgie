97b9c849307767868aa6a4f00e87db7a7f0a4d11
Trevor Guo <trevorguo@gmail.com>
2021-08-03 14:36:15 -0700

    Considered other states ble manager could be in. Printing out services and characteristics

--- src/BluetoothFunctions/Scanner.js
+++ src/BluetoothFunctions/Scanner.js
@@ -4,34 +4,53 @@ const manager = new BleManager();
 
 export const scan = function scan() {
   const subscription = manager.onStateChange(state => {
-    if (state === 'PoweredOn') {
-      manager.startDeviceScan(
-        null,
-        {allowDuplicates: false},
-        (error, device) => {
-          if (error) {
-            console.log(error);
-          }
-          if (
-            device.name === 'ESP32' ||
-            device.name === 'Shaker' ||
-            device.name === 'Trevor’s MacBook Pro'
-          ) {
-            console.log(device.id, device.name);
-            manager.stopDeviceScan();
-            device
-              .connect()
-              .then(device => {
-                return device.discoverAllServicesAndCharacteristics();
-              })
-              .then(() => console.log('Connected'))
-              .catch(error => console.log('Failed to connect'));
-          }
-        },
-      );
-      subscription.remove();
-    } else {
-      console.log('PoweredOff');
+    switch (state) {
+      case 'PoweredOn':
+        manager.startDeviceScan(
+          null,
+          {allowDuplicates: false},
+          (error, device) => {
+            if (error) {
+              console.log(error);
+            }
+            if (
+              device.name === 'ESP32' ||
+              device.name === 'Shaker' ||
+              device.name === 'LE-Little Miss Dynamite'
+            ) {
+              console.log(device.id, device.name);
+              manager.stopDeviceScan();
+              device
+                .connect()
+                .then(() => {
+                  return device.discoverAllServicesAndCharacteristics();
+                })
+                .then(() => console.log('Connected'))
+                .then(() => {
+                  deviceServices = device.services();
+                  return deviceServices;
+                })
+                .then(() => {
+                  console.log(deviceServices);
+                })
+                .catch(error => console.log(error));
+            }
+          },
+        );
+        subscription.remove();
+        break;
+      case 'PoweredOff':
+        console.log('Bluetooth is turned off');
+        break;
+      case 'Unauthorized':
+        console.log('Bluetooth is not authorized for use in this app');
+        break;
+      case 'Unsupported':
+        console.log('Bluetooth LE is not supported on this device');
+        break;
+      default:
+        console.log('Unknown state');
+        break;
     }
   }, true);
 };
